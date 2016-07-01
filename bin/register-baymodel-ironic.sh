2ba099341c949abe76560ee085a3301bd3eb1fd8
OTSUKA, Yuanying <yuanying@fraction.jp>
2016-07-01 10:21:10 +0900

    Update ironic baymodel

--- bin/register-baymodel-ironic.sh
+++ bin/register-baymodel-ironic.sh
@@ -4,7 +4,7 @@ source ~/devstack/openrc demo demo
 magnum baymodel-create --name kubernetes --keypair-id default \
                        --server-type bm \
                        --external-network-id public \
-                       --fixed-network private \
+                       --fixed-subnet private-subnet \
                        --image-id fedora-k8s \
                        --flavor-id baremetal \
                        --network-driver flannel \
